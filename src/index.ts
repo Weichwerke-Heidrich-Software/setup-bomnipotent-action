e3aad7ee3a2b69e21b9a888ee3cd8d535cae85c9
Simon Heidrich <info@wwh-soft.com>
2025-05-24 09:05:10 +0200

    Began downloading client.

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,23 @@
 import * as core from '@actions/core';
-import * as github from '@actions/github';
+import * as toolcache from '@actions/tool-cache';
 
 try {
   const versionToInstall: string = core.getInput('version');
   console.log(`Installing ${versionToInstall}!`);
-  
-  // TODO: Actually install the version of the software here.
-  core.setOutput("version", versionToInstall);
 
-  const payload: string = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+  const os: string = process.platform;
+  console.log(`Running on OS: ${os}`);
+
+  const extension: string = os === 'win32' ? '.exe' : '';
+  const url: string = `https://www.bomnipotent.de/downloads/raw/${versionToInstall}/${os}/bomnipotent_client${extension}`;
+  console.log(`Downloading from URL: ${url}`);
+  // const clientPath: string = await toolcache.downloadTool(url);
+
+  // console.log(`Adding "${clientPath}" to the PATH`);
+  // core.addPath(clientPath)
+
+  // TODO: Actually install the version of the software here.
+  // core.setOutput("version", versionToInstall);
 } catch (error) {
   core.setFailed((error as Error).message);
 }
